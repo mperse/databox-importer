42d6f04be9285c612625531456feb8a5ec49430c
mperse <matej.perse@sinergise.com>
2021-05-29 22:24:20 +0200

    UTF-8 encoding and explicit jaxb support

--- src/main/java/databox/importer/services/core/impl/SystemStatusImpl.java
+++ src/main/java/databox/importer/services/core/impl/SystemStatusImpl.java
@@ -1,13 +1,9 @@
 package databox.importer.services.core.impl;
 
-import java.net.Proxy;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import databox.importer.services.core.resource.SystemStatusService;
 
@@ -19,15 +15,7 @@ public class SystemStatusImpl implements SystemStatusService {
 	@Context
 	protected HttpServletRequest request;
 
-	ObjectMapper mapper = new ObjectMapper();
-	Proxy proxy = null;
-
 	public SystemStatusImpl() {
-	}
-
-	@Override
-	public Response signedResponseRedirect() {
-		return null;
 	}
 
 	@Override
